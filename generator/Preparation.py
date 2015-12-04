5bc7a2d4bf8702d0410e20246f7e8c93ec3d0cfd
vk4arm <vk4arm@gmail.com>
2015-12-04 19:56:47 +0300

    Update Preparation.py
    Немного отрефакторил. Работает под 2.7!

--- generator/Preparation.py
+++ generator/Preparation.py
@@ -5,17 +5,22 @@ import random
 import json
 
 dict_of_lists = {}
-
+corppath = "corpus"
+list_plk = "list.pkl"
+output_file = "generated.txt"
+filelist = []
 
 def add_to_dict_(key, word):
-    if dict_of_lists.has_key(key) :
-        dict_of_lists.get(key).append(word)
+    "Add word to dictionary of lists"
+    if key in dict_of_lists:
+        dict_of_lists[key].append(word)
     else:
         dict_of_lists[key] = [word]
 
 
 
 def add_to_dict(prev_2, prev_1, word, wordtype):
+    "Add to dictionary"
     if prev_2 !=  '__' :
         add_to_dict_(prev_2+'~'+prev_1 + '~' + wordtype, word)
     add_to_dict_(prev_1+ '~' + wordtype, word)
@@ -24,45 +29,43 @@ def add_to_dict(prev_2, prev_1, word, wordtype):
 
 
 def startOfBlock(tokens):
-    for i in xrange(500, 3000) :
+    "Blocks start. If no '.' in the block - return: -1"
+    for i in range(500, 3000) :
         if tokens[i] == '.' :
             return i + 1
     return -1
 
 
 
-corppath = "corpus"
-
-
 def is_punctuation(param):
+    "Checking for punctuation"
     return not param[0].isalpha()
 
 def file_content(file_name):
+    "Reading content"
     out =  open(file_name).read().decode('utf-8')
-    out = out.replace( u'\u2018', "'")
-    out = out.replace( u'\u2019', "'")
-    out = out.replace( u'\u201c', '"')
-    out = out.replace( u'\u201d', '"')
+    out = out.replace( u'\u2018', "'").replace( u'\u2019', "'").replace( u'\u201c', '"').replace( u'\u201d', '"')
     return out.encode('ascii', 'ignore')
 
 def prepare_corpus():
+  "Prepare the text corpus"
   for root, dirs, files in os.walk(corppath, topdown=False):
       for name in files:
           print(os.path.join(root, name))
-          print len(dict_of_lists)
+          print (len(dict_of_lists))
           str_input = file_content(os.path.join(root, name))
           tokens = word_tokenize(str_input)
           if len(tokens) < 3000 :
               continue
-          print len(tokens)
+          print (len(tokens))
           pt = pos_tag(tokens)
-          print len(pt)
+          print (len(pt))
           start_position = startOfBlock(tokens)
           if start_position == -1:
               continue;
           prev_2 = '__'
           prev_1 = '__'
-          for i in xrange(start_position, len(tokens)):
+          for i in range(start_position, len(tokens)):
               if is_punctuation(pt[i][1]) : #punctuation
                   if tokens[i] == '.': #end of sentense
                       prev_2 = '__'
@@ -74,24 +77,25 @@ def prepare_corpus():
 
 
 def save_to_disk():
-    output = open('list.pkl', 'wb')
+    "Saving results"
+    output = open(list_plk, 'wb')
     json.dump(dict_of_lists, output)
     output.close()
 
 
-
 def get_random_file_to_grammar():
-    filelist = []
-    for root, dirs, files in os.walk(corppath, topdown=False):
-        for name in files:
-            filelist.append(os.path.join(root, name))
+    "Getting random file"
+    if len(filelist) == 0:
+        for root, dirs, files in os.walk(corppath, topdown=False):
+            map(lambda name: filelist.append(os.path.join(root, name)), files)
     return random.choice(filelist)
 
 
 def generate_pseudiotext():
+    "Generate text"
     filename = get_random_file_to_grammar()
-    str = file_content(filename)
-    tokens = word_tokenize(str)
+    string = file_content(filename)
+    tokens = word_tokenize(string)
     start_position = startOfBlock(tokens)
     pt = pos_tag(tokens)
     prev_2 = '__'
@@ -99,7 +103,7 @@ def generate_pseudiotext():
     words = 0
     is_sentence_start = True
 
-    output = open("generated.txt", "w")
+    output = open(output_file, "w")
 
     row_length = [90]
     def write_up_first_if(word, to_up):
@@ -117,14 +121,15 @@ def generate_pseudiotext():
         if wordtype == 'DT' :
             return curword.lower()
         key = prev_2 + '~' + prev_1 + '~' + wordtype
-        if dict_of_lists.has_key(key):
+        if key in dict_of_lists:
             return  random.choice(dict_of_lists[key])
         key = prev_1 + '~' + wordtype
-        if dict_of_lists.has_key(key):
+        if key in dict_of_lists:
             return  random.choice(dict_of_lists[key])
         return curword.lower()
 
     lines  = [random.randint(2, 10)]
+    
     def random_paragraf():
         #print lines[0]
         if lines[0] == 0 :
@@ -136,7 +141,7 @@ def generate_pseudiotext():
             lines[0] = lines[0] - 1
 
     output.write('\t\t'.encode("ascii"))
-    for i in xrange(start_position, len(tokens)):
+    for i in range(start_position, len(tokens)):
         if is_punctuation(pt[i][1]) : #punctuation
              write_up_first_if(tokens[i], False)
              if tokens[i] == '.' : #end of sentense
@@ -160,3 +165,4 @@ def generate_pseudiotext():
 if __name__ == '__main__' :
     prepare_corpus()
     generate_pseudiotext()
+
